61afdef1bc09a33a5994d30cc99c2fd4c67a44e1
rankaifeng <18202395273@163.com>
2020-07-28 14:11:18 +0800

    com

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -4,6 +4,12 @@ import '../less/index.less';
 import ReactMarkdown from 'react-markdown'
 import MarkNav from 'markdown-navbar';
 import Author from '../components/Author';
+import marked from 'marked'
+import hljs from "highlight.js";
+import 'highlight.js/styles/monokai-sublime.css';
+import Tocify from '../components/tocify.tsx'
+// import 'highlight.js/styles/monokai-sublime.css';
+
 let markdown = '# P01:课程介绍和环境搭建\n' +
   '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
   '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
@@ -39,26 +45,50 @@ let markdown = '# P01:课程介绍和环境搭建\n' +
   '>>> cccccccccc\n\n' +
   '``` var a=11; ```'
 const Detail = () => {
+
+  const tocify = new Tocify()
+  const renderer = new marked.Renderer();
+  renderer.heading = function (text, level, raw) {
+    const anchor = tocify.add(text, level);
+    return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+  };
+
+  marked.setOptions({
+
+    renderer: renderer,
+
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+
+  });
+
+  let html = marked(markdown)
   return (<div>
     <Row className='comm-main' type='flex' justify='center'>
       <Col className='comm-left' xs={24} sm={24} md={16} lg={18} lx={10}>
-        <div className="detailed-content" >
-          <ReactMarkdown
-            source={markdown}
-            escapeHtml={true}
-          />
-        </div>
+        <div className="detailed-content" dangerouslySetInnerHTML={{ __html: html }} />
+
       </Col>
       <Col className='comm-right' xs={0} sm={0} md={7} lg={5} lx={14}>
         <Author />
         <Affix>
           <div className="detailed-nav comm-box">
             <div className="nav-title">文章目录</div>
-            <MarkNav
+            {tocify && tocify.render()}
+            {/* <MarkNav
               className="article-menu"
               source={markdown}
               ordered={false}
-            />
+            /> */}
           </div>
         </Affix>
       </Col>
